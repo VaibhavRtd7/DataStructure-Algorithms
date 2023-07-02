066b35de8dda3a0db4977bb8660dd052e934db16
Vaibhav Rathod <93068189+VaibhavRtd7@users.noreply.github.com>
2023-07-02 17:02:46 +0530

    BubbleSort

--- Sorting Algorithms/05_BubbleSort.cpp
+++ Sorting Algorithms/05_BubbleSort.cpp
@@ -1,1 +1,49 @@
+#include<bits/stdc++.h>
+// #include<iostream>
+
+using namespace std;
+
+void printArray(int arr[], int n) {
+    for(int i=0; i<n; i++)
+      cout << arr[i] << " ";
+    cout << endl;
+}
+
+void swap(int *a, int *b) {
+
+    int tp = *a;
+        *a = *b;
+        *b = tp;
+}
+
+void BubbleSort(int arr[], int n) {
+    
+    for(int i=0; i<n-1; i++) {
+        for(int j=0; j<n-i-1; j++) {
+            if(arr[j] > arr[j+1]) {
+                swap(&arr[j], &arr[j+1]);
+            }
+        }
+    }
+}
+
+int main() {
+   
+     int arr[] = {20, 10, 50, 30, 40, 00};
+     int n = sizeof(arr)/sizeof(arr[0]);
+
+     cout << "Array Before Sorting." << endl;
+     printArray(arr, n);
+
+     BubbleSort(arr, n);
+     cout << "Array After Sorting." << endl;
+     printArray(arr, n);
+
+    return 0;
+}
+
+
+// Stable Sort
+// worst case -  n^2
+// best case - n
 
