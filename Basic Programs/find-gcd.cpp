a3daf76d97bab20b41ff3cbea2fa518a66785dd8
Vaibhav Rathod <93068189+VaibhavRtd7@users.noreply.github.com>
2024-08-19 18:35:55 +0530

    Update find-gcd.cpp

--- Basic Programs/find-gcd.cpp
+++ Basic Programs/find-gcd.cpp
@@ -1,25 +1,19 @@
 #include <iostream> 
+#include <bits/stdc++.h>
 using namespace std; 
 
 int gcd(int a, int b) 
 { 
-	if (a == 0) 
-		return b; 
-		
 	if (b == 0) 
-		return a; 
-
-	if (a == b) 
-		return a; 
-
-	if (a > b) 
-		return gcd(a - b, b); 
-	return gcd(a, b - a); 
+	   return a; 
+	
+   return gcd(b, a % b);
 } 
-
+    
 int main() 
 { 
 	int a = 98, b = 56; 
+// 	cout << "Function :- " << __gcd(98, 14) << endl;
 	cout << "GCD of " << a << " and " << b << " is "
 		<< gcd(a, b); 
 	return 0; 
