0e33bd4c59b4a51bfa59e78ac5ab2f1fef575bfd
Vaibhav Rathod <93068189+VaibhavRtd7@users.noreply.github.com>
2024-08-19 18:39:47 +0530

    updated find-gcd.cpp to find-gcd-lcm.cpp

--- Basic Programs/find-gcd-lcm.cpp
+++ Basic Programs/find-gcd-lcm.cpp
@@ -4,17 +4,26 @@ using namespace std;
 
 int gcd(int a, int b) 
 { 
+
 	if (b == 0) 
-	   return a; 
-	
+		return a; 
+
    return gcd(b, a % b);
 } 
+
+int lcm(int a, int b) {
+    return (a*b) / gcd(a,b);
+}
     
+// lcm * gcd = a*b
+
 int main() 
 { 
-	int a = 98, b = 56; 
-// 	cout << "Function :- " << __gcd(98, 14) << endl;
+	int a = 15, b = 10; 
+	cout << "LCM of " << a << " and " << b << " is "
+		<< lcm(a, b) << endl; 
+	
 	cout << "GCD of " << a << " and " << b << " is "
-		<< gcd(a, b); 
+		<< gcd(a, b) << endl;
 	return 0; 
 }
