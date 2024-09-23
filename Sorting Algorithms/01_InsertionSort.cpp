67e4fea7a33072a6ef3bb5f30da8633c7e68abd6
Vaibhav Rathod <93068189+VaibhavRtd7@users.noreply.github.com>
2024-09-23 12:57:44 +0530

    Update 01_InsertionSort.cpp

--- Sorting Algorithms/01_InsertionSort.cpp
+++ Sorting Algorithms/01_InsertionSort.cpp
@@ -3,7 +3,7 @@
 
 void InsertionSort(int arr[], int n) {
 
-  for (int i = 0; i <= n - 1; i++) {
+  for (int i = 0; i < n-1; i++) {
         int j = i;
         while (j > 0 && arr[j - 1] > arr[j]) {
             int temp = arr[j - 1];
