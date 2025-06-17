8ab7bd66c826d5d17638f78f1c4770b547bcb3a4
Vaibhav Rathod <93068189+VaibhavRtd7@users.noreply.github.com>
2025-06-17 23:43:47 +0530

    Update 1_reverseString.java

--- OOPS/Java_programs/1_reverseString.java
+++ OOPS/Java_programs/1_reverseString.java
@@ -8,6 +8,10 @@ public class ReverseString {
         }
         return reversed.toString();
     }
+
+    // public static String reverse(String str) {
+    //     return new StringBuilder(str).reverse().toString();
+    // }
     
     public static void main(String args[]) {
        System.out.println(reverse("Vaibhav")); // vahbiaV
